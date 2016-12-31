89cfd550914e8690469fd6ee05e4d85a87a2a887
Gabriel Dunne <gdunne@quilime.com>
2016-12-31 12:57:03 -0800

    Updating scripts

--- include/monome/start-serialosc.sh
+++ include/monome/start-serialosc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Stopping any instance of serialosc before starting again."
-./stop-serialosc.sh
-./serialosc-device /dev/ttyUSB0 &
+/usbdrive/include/monome/stop-serialosc.sh
+/usbdrive/include/monome/serialosc-device /dev/ttyUSB0 &
 echo "Starting serialosc-device on /dev/ttyUSB0"
