90963ba697c0cb7a06767f2062311695471a1a76
Gabriel Dunne <gdunne@quilime.com>
2017-01-01 23:18:08 -0800

    Updating links

--- include/monome/createlinks.sh
+++ include/monome/createlinks.sh
@@ -6,12 +6,13 @@ set -x
 /root/scripts/remount-rw.sh
 
 # create links to libraries with symlinks
-
+mkdir -p /usr/local/lib
 ln -s /usbdrive/include/monome/lib/monome /usr/local/lib/
 ln -s /usbdrive/include/monome/lib/libmonome.so /usr/local/lib/
 ln -s /usbdrive/include/monome/lib/libmonome.so.1 /usr/local/lib/
 ln -s /usbdrive/include/monome/lib/libmonome.so.1.4.0 /usr/local/lib/
 
+mkdir -p /usr/local/bin
 ln -s /usbdrive/include/monome/monomeserial /usr/local/bin/
 ln -s /usbdrive/include/monome/serialoscd /usr/local/bin/
 ln -s /usbdrive/include/monome/serialosc-detector /usr/local/bin/
