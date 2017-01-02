7c06ecd8e5b9fb278526bb1690004c2ac1b465fa
Gabriel Dunne <gdunne@quilime.com>
2017-01-01 17:19:41 -0800

    Install

--- install.sh
+++ install.sh
@@ -6,16 +6,19 @@ set -x
 echo "/usr/local/lib" > /etc/ld.so.conf.d/usrlocal.conf
 
 mkdir tmp
-cd tmp
+pushd tmp > /dev/null
+
 git clone https://github.com/monome/libmonome.git
 git clone https://github.com/monome/serialosc.git
 
-cd libmonome
+pushd libmonome > /dev/null
 ./waf configure
 ./waf
 ./waf install
 
-cd ../serialosc
+popd > /dev/null
+
+pushd serialosc > /dev/null
 git submodule init && git submodule update
 ./waf configure
 ./waf
